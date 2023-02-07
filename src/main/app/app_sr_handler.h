b1cc3069f401de05e989e92540d031af85394596
Steffan Lloyd <steffan.lloyd@icloud.com>
2023-02-07 15:57:13 -0500

    Updates

--- src/main/app/app_sr_handler.h
+++ src/main/app/app_sr_handler.h
@@ -12,11 +12,11 @@
 #define LED_RED 39
 #define LED_GREEN 40
 #define LED_BLUE 41
-#define GPIO_HEAD_UP 10
-#define GPIO_HEAD_DOWN 9
-#define GPIO_LEG_UP 11
-#define GPIO_LEG_DOWN 43
-#define GPIO_BED_UP 13
-#define GPIO_BED_DOWN 44
+#define GPIO_HEAD_DOWN 10
+#define GPIO_HEAD_UP 9
+#define GPIO_LEG_DOWN 11
+#define GPIO_LEG_UP 43
+#define GPIO_BED_DOWN 13
+#define GPIO_BED_UP 44
 
 void sr_handler_task(void *pvParam);
